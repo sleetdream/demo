39478af0fb378509210322b4af4e58f930b0dc4c
sleetdream <sleetdream@live.cn>
2019-07-24 17:09:34 +0800

    补全注释

--- quartz/src/main/java/com/example/quartz/configs/QuartzConfig.java
+++ quartz/src/main/java/com/example/quartz/configs/QuartzConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 定时任务配置
+ * 参考 https://blog.csdn.net/gnail_oug/article/details/80825302
  */
 @Configuration
 public class QuartzConfig {
@@ -18,8 +19,10 @@ public class QuartzConfig {
     @Bean
     public Trigger testQuartzTrigger1() {
         return TriggerBuilder.newTrigger().forJob(testQuartz1())
-                .withIdentity("HelloTask")//要执行的文件
-                .withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?"))//cron表达式，每5秒执行一次
+                //要执行的文件
+                .withIdentity("HelloTask")
+                //cron表达式，每5秒执行一次，参考 https://www.cnblogs.com/dubhlinn/p/10740838.html
+                .withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?"))
                 .build();
     }
 
